6e666bf87c221649e22726f1fe722c0f162a62e8
backgtx <wesleyafc2010@gmail.com>
2021-08-03 20:45:57 -0300

    params types

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ const express = require('express')
 //const bodyParser = require('body-parser')
 
 const app = express()
-
+app.use(express.json())
 
 app.get('/courses',(req,res)=>{
+    const query = req.query
+    console.log(query)
     return res.json(["curse 1","curse 2","curse 3"])
 })
 
 app.post('/courses',(req,res)=>{
+    const body = req.body
+    console.log(body)
     return res.json(["curse 1","curse 2","curse 3","curse 4"])
 })
 
 app.put('/courses/:id',(req,res)=>{
+    const {id} = req.params
+    console.log(id)
     return res.json(["curse 7","curse 2","curse 3","curse 4"])
 
 })
